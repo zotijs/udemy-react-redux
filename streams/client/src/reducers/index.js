fb1d6b214982f44e8b370239e7b8e7d323baaf11
Sotiris Tsiamouris <tsot85@gmail.com>
2019-04-20 16:05:00 +0300

    streams/ working with redux-form

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'; //rename redux-form package reducer for ease of use
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer //always assign the redux-form reducer to a key of form
 });
